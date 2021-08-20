4cf4ed5baee907cd52795dedf91850f0dc8f4d5c
n4b3ts3 <n4b3ts3@yahoo.com>
2021-08-20 08:52:22 -0400

    init JustAntoher Blockchain JAChain Project

---
+++ client/JustAnotherChain.java
@@ -0,0 +1,11 @@
+import com.nerox.client.modules.XSAce;
+import com.nerox.client.callbacks.IXSAceCallback;
+
+public class JustAnotherChain{
+    public JustAnotherChain(){
+    }
+
+    public static void main(String [] args){
+        JustAnotherChain jac = new JustAnotherChain();
+    }
+}
